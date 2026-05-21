3f6e4749769cbea1042a404ebe47d355b96d6a9c
Arun-2601-2007 <nalabalaaruneswarreddy2601@gmail.com>
2026-05-21 16:35:31 +0530

    Added Project Files

--- src/components/footer.py
+++ src/components/footer.py
@@ -2,20 +2,20 @@ import streamlit as st
 
 def footer_home():
 
-    logo_url = "https://ninahands.in/wp-content/uploads/2019/12/NIT-calicut-LOGO.jpg"
-    st.markdown("""
-        <div style="display:flex; flex-direction:column; align-items:center; justify-content:center; margin-top:2rem; gap:6px;">
+    logo_url = "https://images.seeklogo.com/logo-png/45/1/national-institute-of-technology-calicut-logo-png_seeklogo-458279.png"
+    st.markdown(f"""
+        <div style="display:flex; flex-direction:column; align-items:center; justify-content:center; margin-top:2rem; gap:3px;">
             <p style='font-weight:bold; color:white;'>Created with ❤️ by</p>
-            <img src='{logo_url}' style='max-height:25px' />
+            <img src='{logo_url}' style='max-height:50px' />
         </div>
     """, unsafe_allow_html=True)
 
 def footer_dashboard():
 
-    logo_url = "https://ninahands.in/wp-content/uploads/2019/12/NIT-calicut-LOGO.jpg"
-    st.markdown("""
-        <div style="display:flex; flex-direction:column; align-items:center; justify-content:center; margin-top:2rem; gap:6px;">
+    logo_url = "https://images.seeklogo.com/logo-png/45/1/national-institute-of-technology-calicut-logo-png_seeklogo-458279.png"
+    st.markdown(f"""
+        <div style="display:flex; flex-direction:column; align-items:center; justify-content:center; margin-top:2rem; gap:3px;">
             <p style='font-weight:bold; color:black;'>Created with ❤️ by</p>
-            <img src='{logo_url}' style='max-height:25px' />
+            <img src='{logo_url}' style='max-height:50px' />
         </div>
     """, unsafe_allow_html=True)
